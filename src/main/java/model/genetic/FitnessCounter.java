68200b98e1685ee4e21a6b87f778ec6375ec77aa
Dan Edmunds <dan.edmunds@qlik.com>
2016-12-03 13:20:51 -0500

    mavenify

--- src/main/java/model/genetic/FitnessCounter.java
+++ src/main/java/model/genetic/FitnessCounter.java
@@ -1,24 +1,30 @@
 package model.genetic;
 
 public class FitnessCounter {
-	
+
 	private int[] counters;
-	
+
 	public FitnessCounter(int maxValue) {
 		counters = new int[maxValue];
+		System.out.println(maxValue);
 	}
-	
+
+	/**
+	 * Notifies the counter that the specified value has appeared
+	 * 
+	 * @param value
+	 */
 	public void occurred(int value) {
-		assert(value < counters.length);
+		assert (value - 1 < counters.length);
 		counters[value - 1]++;
 	}
-	
+
 	public int getFitness() {
 		int fitness = 0;
 		for (int i = 0; i < counters.length; ++i)
 			if (counters[i] > 1)
 				fitness += counters[i] - 1;
-		
+
 		return fitness;
 	}
 
